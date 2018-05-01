60f469aadfcaea39adff7b699f4a15fede97684a
unknown <bgklein@outlook.com>
2018-05-01 16:06:16 -0400

    End of Tuesday progress push

--- BowlingScore/Program.cs
+++ BowlingScore/Program.cs
@@ -11,31 +11,64 @@ namespace BowlingScore
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Dude's Lanes");
-            int frame = 0;
             int ballOne = 0;
-            int ballOneScore = 0;
             int ballTwo = 0;
-            int ballTwoScore = 0;
-            do
+            int frameScore = 0;
+            int[] score = new int[10];
+            for(int i = 1; i<10; i++)
             {
-                Console.WriteLine("What frame are you in?");
-                frame = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("It is frame " + i);
                 Console.WriteLine("What did you roll on your first ball?");
                 ballOne = Int32.Parse(Console.ReadLine());
-                if (ballOne <= 9)
-                {
-                    ballOneScore = ballOne;
-                }
-                else if (ballOne == 10)
-                {
-                    Console.WriteLine("You are throwing rocks, tonight! Whoo!");
-                }
-                else
-                {
-                    Console.WriteLine("Please try re-entering the number of pins you knocked over with your first ball.");
-                }
+                Console.WriteLine("How many pins did you knock down with your second ball?");
+                ballTwo = Int32.Parse(Console.ReadLine());
+                frameScore = FrameScore(ballOne, ballTwo);
+                Console.WriteLine("You scored " + frameScore + " in frame " + i + ".");
+                score[i] = frameScore;
+            } 
+            Console.WriteLine("It is the 10th frame. Let's make it a good one, el Duderino!");
+        }
 
-            } while (frame > 0 && frame < 10);
+        //Methods
+        static int FrameScore(int ballOne, int ballTwo)
+        {
+            int frameSubtotal = 0;
+            if (ballOne <= 9 && ballOne+ballTwo <10)
+            {
+                frameSubtotal = ballOne+ballTwo;
+            }
+            else if (ballOne == 10)
+            {
+                Console.WriteLine("You are throwing rocks, tonight! Whoo!");
+                Strike(ballOne, ballTwo);
+                //continue;
+            }
+            else if (ballOne < 0 || ballOne > 10 )
+            {
+                Console.WriteLine("Please try re-entering the number of pins you knocked over with your first ball.");
+                Console.WriteLine("This isn't Nam, Smokey. There are rules.");
+                ballOne = Int32.Parse(Console.ReadLine());
+            }
+
+            else if (ballTwo + ballOne == 10)
+            {
+                Spare(ballOne);
+            }
+            else if (ballTwo < 0 || ballTwo > 10)
+            {
+                Console.WriteLine("Please try re-entering the number of pins you knocked over with your second ball.");
+                Console.WriteLine("This isn't Nam, Smokey. There are rules.");
+                ballTwo = Int32.Parse(Console.ReadLine());
+            }
+            return frameSubtotal;
+        }
+        static int Spare(int ballOne)
+        {
+            return 10;
+        }
+        static int Strike(int ballOne, int ballTwo)
+        {
+            return 10 + ballOne + ballTwo;
         }
     }
 }
